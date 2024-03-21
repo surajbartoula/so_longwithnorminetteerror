3cf8756337bf22c99bd318ee232464187f26531d
surajbartoula <suraj_bartoula@yahoo.com>
2024-03-21 08:40:55 +0400

    reay for submission

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/03/20 20:53:56 by sbartoul          #+#    #+#              #
-#    Updated: 2024/03/20 20:54:00 by sbartoul         ###   ########.fr        #
+#    Updated: 2024/03/21 08:39:28 by sbartoul         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@ CC := gcc
 
 CFLAGS := -Wall -Wextra -Werror -Iheaders/
 
-SOURCE := game_logic/*.c
+SOURCE := logic/*.c
 GETNEXTLINE := get_next_line/*.c
 FTPRINTF := ft_printf/*.c
 LIBRARY := -Lmlx -lmlx -framework OpenGL -framework AppKit
--- logic/graphics.c
+++ logic/graphics.c
@@ -6,7 +6,7 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 18:13:30 by sbartoul          #+#    #+#             */
-/*   Updated: 2024/03/19 10:38:35 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/03/21 08:35:03 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,15 @@ void	place_images_in_game(t_play *game)
 	int	y_pos;
 
 	game->floor = mlx_xpm_file_to_image(game->mlx,
-			"game_images/water.xpm", &x_pos, &y_pos);
+			"images/water.xpm", &x_pos, &y_pos);
 	game->barrier = mlx_xpm_file_to_image(game->mlx,
-			"game_images/tiles.xpm", &x_pos, &y_pos);
+			"images/tiles.xpm", &x_pos, &y_pos);
 	game->player = mlx_xpm_file_to_image(game->mlx,
-			"game_images/dolphin.xpm", &x_pos, &y_pos);
+			"images/dolphin.xpm", &x_pos, &y_pos);
 	game->exit = mlx_xpm_file_to_image(game->mlx,
-			"game_images/exit.xpm", &x_pos, &y_pos);
+			"images/exit.xpm", &x_pos, &y_pos);
 	game->collectable = mlx_xpm_file_to_image(game->mlx,
-			"game_images/prey.xpm", &x_pos, &y_pos);
+			"images/prey.xpm", &x_pos, &y_pos);
 }
 
 void	add_characters(t_play *game)
