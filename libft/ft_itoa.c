5c6556ceb978232b001aacb3dee723b098fba6b8
surajbartoula <suraj_bartoula@yahoo.com>
2024-04-16 20:30:28 +0400

    fixed the issue in ft_itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: sbartoul <sbartoul@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 16:15:43 by sbartoul          #+#    #+#             */
-/*   Updated: 2023/12/27 13:03:08 by sbartoul         ###   ########.fr       */
+/*   Updated: 2024/04/16 20:29:20 by sbartoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,11 @@
 static int	ft_len(long n)
 {
 	size_t	len;
-	int		neg;
 
 	len = 0;
-	neg = 0;
 	if (n < 0)
 	{
 		len++;
-		neg++;
 		n = -n;
 	}
 	while (n >= 1)
